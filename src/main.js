56632575cb6811d7df147c5b60fcfd63f5697edb
Paul Jomy <pauljomy@users.noreply.github.com>
2026-02-13 21:36:49 +0530

    email error validation completed

--- src/main.js
+++ src/main.js
@@ -1,16 +1,29 @@
 const form = document.getElementById("form");
 const emailInputBtnEl = document.getElementById("email-input");
 const errorBtnLabelEl = document.getElementById("email-error-label");
-const emailPattern = "/^[^\s@]+@[^\s@]+\.[^\s@]+$/";
+const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const data = Object.fromEntries(new FormData(e.target));
   console.log("button clicked");
-  if (!data.email) {
+  if (!data.email || !emailPattern.test(data.email)) {
     errorBtnLabelEl.classList.remove("hidden");
     emailInputBtnEl.classList.add(
+      "bg-input-text-error-bg",
+      "border-error-text",
+      "text-error-text",
+    );
+  }
+});
+
+document.addEventListener("click", (e) => {
+  const btn = e.target.closest("#submit");
+
+  if (!btn) {
+    errorBtnLabelEl.classList.add("hidden");
+    emailInputBtnEl.classList.remove(
       "bg-input-text-error-bg",
       "border-error-text",
       "text-error-text",
